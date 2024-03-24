52c66f74a57e9976e3a80e6a891ce08513c16628
edrickt <edrick.tamayo@zoho.com>
2024-03-24 19:07:52 -0400

    commit

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 class Game:
     @staticmethod
-    def get_game_logs(num_games=2000):
+    def get_game_logs(num_games=164):
         try:
             game_log = pd.read_csv("game_log.csv", index_col=False)
             return game_log
