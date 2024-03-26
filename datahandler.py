80c834d7c0c0433e0f45b55203d747fd9f389f5c
edrickt <edrick.tamayo@zoho.com>
2024-03-25 20:16:44 -0400

    commit

---
+++ datahandler.py
@@ -0,0 +1,36 @@
+from game import Game
+from team import Team
+from helperfunctions import get_seasons
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+
+
+class DataHandler:
+    def __init__(self):
+        self.game_metrics = None
+        self.X = None
+        self.y = None
+        self.team_away = None
+        self.team_home = None
+        self.cur_season = get_seasons()[0]
+        self.columns_to_drop = ["TEAM_NAME_A", "TEAM_NAME_H", "TEAM_ID_A", "TEAM_ID_H", "GP_A", "GP_H", "W_A", "W_H", "L_A", "L_H", "SEASON_A", "SEASON_H", "TOTAL_POINTS"]
+        
+        self._load_data()
+        self._set_X_y()
+        
+    def input_teams_get_X_pred(self):
+        self.team_away = Team.set_team(season=self.cur_season, name=input("Away Team: ").lower())
+        self.team_home = Team.set_team(season=self.cur_season, name=input("Home Team: ").lower())
+        
+        X_pred = pd.concat([self.team_away.stats.add_suffix("_A"), self.team_home.stats.add_suffix("_H")], axis=1).drop(columns=self.columns_to_drop[:-1])
+        return X_pred
+        
+    def _load_data(self, num_years=5):
+        Team.team_stats_to_csv(num_years=num_years)
+        game_logs = Game.get_game_logs(num_years=num_years)
+        self.game_metrics = Game.get_team_metrics_for_games(game_logs)
+        
+    def _set_X_y(self):
+        self.X = self.game_metrics.drop(columns=self.columns_to_drop)
+        self.y = self.game_metrics["TOTAL_POINTS"]
+            
--- main.py
+++ main.py
@@ -1,33 +1,15 @@
-from game import Game
-from team import Team
-from helperfunctions import get_seasons
-import pandas as pd
+from datahandler import DataHandler
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import GridSearchCV
 
 
 if __name__ == "__main__":
-    num_years = 5
-    
-    Team.team_stats_to_csv(num_years=num_years)
-    game_logs = Game.get_game_logs(num_years=num_years)
-    game_metrics = Game.get_team_metrics_for_games(game_logs)
+    dh = DataHandler()
     
     while True:
-            cur_season = get_seasons()[0]
-            
-            team_away = Team.set_team(season=cur_season, name=input("Away Team: ").lower())
-            team_home = Team.set_team(season=cur_season, name=input("Home Team: ").lower())
+            X_pred = dh.input_teams_get_X_pred()
 
-            columns_to_drop = ["TEAM_NAME_A", "TEAM_NAME_H", "TEAM_ID_A", "TEAM_ID_H", "GP_A", "GP_H", "W_A", "W_H", "L_A", "L_H", "SEASON_A", "SEASON_H", "TOTAL_POINTS"]
-
-            X = game_metrics.drop(columns=columns_to_drop)
-            y = game_metrics["TOTAL_POINTS"]
-
-            model = DecisionTreeRegressor(max_depth=3).fit(X, y)
-
-            X_pred = pd.concat([team_away.stats.add_suffix("_A"), team_home.stats.add_suffix("_H")], axis=1).drop(columns=columns_to_drop[:-1])
+            model = DecisionTreeRegressor(max_depth=3).fit(dh.X, dh.y)
             y_pred = model.predict(X_pred)
 
-            print(f"{team_away.info.nickname[0].capitalize()} vs {team_home.info.nickname[0].capitalize()}: {y_pred[0]:.2f} points\n")
+            print(f"{dh.team_away.info.nickname[0].capitalize()} vs {dh.team_home.info.nickname[0].capitalize()}: {y_pred[0]:.2f} points\n")
             
