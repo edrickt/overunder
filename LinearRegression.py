9f873b80fdc9289fc34a45b7d135837b4e6035b7
HopingR <hopingr7@gmail.com>
2024-03-28 11:59:52 -0400

    LinearRegression file

---
+++ LinearRegression.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.tree import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn import metrics 
+
+
+class LinearRegression:
+    def __init__(self):
+
+        self.X = None
+        self.y = None
+        self.model = LinearRegression()
+
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        self.model.fit(X, y) 
+
+    def predict(self, X_pred):
+        y_pred = self.model.predict(X_pred)
+        return y_pred
+    
+    def get_score(self, X_test, y_test):
+
+        y_pred = self.model.predict(X_test)
+        return metrics.accuracy_score(y_test, y_pred)
