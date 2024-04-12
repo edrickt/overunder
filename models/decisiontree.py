49c2abcc17bff99ff1d56de2a1db1d99ffb0cda9
edrickt <edrick.tamayo@zoho.com>
2024-04-12 09:17:01 -0400

    commit

---
+++ models/decisiontree.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics 
+
+
+class DecisionTree:
+    def __init__(self):
+        self.X = None
+        self.y = None
+        self.model = DecisionTreeClassifier()
+
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        self.model.fit(X, y) 
+
+    def predict(self, X_pred):
+        y_pred = self.model.predict(X_pred)
+        return y_pred
+    
+    def get_score(self, X_test, y_test):
+        y_pred = self.model.predict(X_test)
+        return metrics.accuracy_score(y_test, y_pred)
+    
